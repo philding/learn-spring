4fe388b757f3f1b7f378eb4c303e8e7b90ba506a
Qf.Ding <Qf.Ding@yh>
2024-06-18 18:19:09 +0800

    Spring test

--- spring-test-ioc/src/main/java/com/phi/test/BeanFactoryTest.java
+++ spring-test-ioc/src/main/java/com/phi/test/BeanFactoryTest.java
@@ -4,12 +4,23 @@ import com.phi.dao.UserDao;
 import com.phi.service.UserService;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
 
+/**
+ * 1. 概念定义
+ * BeanDefinition：Bean元数据描述，Bean在Spring IOC容器中的抽象，是Spring的一个核心概念
+ * DefaultListableBeanFactory ：Spring IOC容器的实现，可以作为一个独立使用的容器， Spring IOC容器的始祖
+ * XmlBeanFactory：继承自DefaultListableBeanFactory，与其不同点在于XmlBeanFactory中使用了自定义的XML读取器XmlBeanDefinitionReader，实现了个性化的BeanDefinitionReader读取
+ * ApplicationContext: 高级容器定义接口，基于BeanFactory添加了扩展功能，如ResourceLoader、MessageSource、ApplicationEventPublisher等
+ */
 public class BeanFactoryTest {
     public static void main(String[] args) {
 
         //创建工厂对象
         DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory ();
+
+        XmlBeanFactory bf = new XmlBeanFactory(new ClassPathResource("beans.xml"));
 
         //创建一个读取器(xml文件)
         XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader (beanFactory);
