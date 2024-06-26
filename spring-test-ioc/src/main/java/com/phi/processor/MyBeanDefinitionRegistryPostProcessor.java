80cf2d42d3fd3b55c91cbed4357595e3de77ad46
QfDing <QfDing@foxmial.com>
2024-06-26 18:24:12 +0800

    learn

---
+++ spring-test-ioc/src/main/java/com/phi/processor/MyBeanDefinitionRegistryPostProcessor.java
@@ -0,0 +1,20 @@
+package com.phi.processor;
+
+import org.springframework.beans.BeansException;
+
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.beans.factory.support.BeanDefinitionRegistryPostProcessor;
+
+public class MyBeanDefinitionRegistryPostProcessor implements BeanDefinitionRegistryPostProcessor {
+
+    @Override
+    public void postProcessBeanDefinitionRegistry(BeanDefinitionRegistry beanDefinitionRegistry) throws BeansException {
+
+    }
+
+    @Override
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory configurableListableBeanFactory) throws BeansException {
+
+    }
+}
