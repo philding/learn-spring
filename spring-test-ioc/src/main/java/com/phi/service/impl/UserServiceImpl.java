2fbcf970bcc9e09edcb81ca127c41385a2c3162f
QfDing <QfDing@foxmial.com>
2024-06-28 15:42:46 +0800

    learn

--- spring-test-ioc/src/main/java/com/phi/service/impl/UserServiceImpl.java
+++ spring-test-ioc/src/main/java/com/phi/service/impl/UserServiceImpl.java
@@ -2,20 +2,18 @@ package com.phi.service.impl;
 
 import com.phi.dao.UserDao;
 import com.phi.service.UserService;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 
 
-
-
-
-
+@Slf4j
 public class UserServiceImpl implements UserService {
 
+    //Set方法注入
+    @Setter
     private UserDao userDao;
 
-    //Set方法注入
-    public void setUserDao(UserDao userDao) {
-        this.userDao = userDao;
-    }
+    private String name;
 
     public void show() {
         System.out.println("UserServiceImpl Show:");
