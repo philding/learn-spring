e33a726e4d7ca7418b0342f5cb102a9b698ca656
QfDing <QfDing@foxmial.com>
2024-07-01 15:26:09 +0800

    learn aop

--- spring-test-aop/src/main/java/org/example/Main.java
+++ spring-test-aop/src/main/java/org/example/Main.java
@@ -1,6 +1,12 @@
 package org.example;
 
+import org.example.service.UserInfoService;
+
 public class Main {
+
+    private UserInfoService userInfoService;
+
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
